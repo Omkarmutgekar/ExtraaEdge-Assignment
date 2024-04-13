f7223c50bf1444d011161ecda267389c33213e01
omkar mutgekar <95881428+Omkarmutgekar@users.noreply.github.com>
2024-04-14 00:38:44 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,4 @@
-// // redux/store.js
-// import { configureStore } from '@reduxjs/toolkit';
-// import usersReducer from './reducers/usersSlice';
-// import avatarsReducer from './reducers/avatarsSlice';
 
-// const store = configureStore({
-//   reducer: {
-//     users: usersReducer,
-//     avatars: avatarsReducer,
-//   },
-// });
-
-// export default store;
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from '../redux/Slices/usersSlice';
 
